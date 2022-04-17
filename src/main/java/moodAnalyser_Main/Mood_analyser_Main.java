a33423b2d386e5fae995e6ad93f8c2c4295e04ae
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-04-17 16:59:00 +0530

    Refractor case added

--- src/main/java/moodAnalyser_Main/Mood_analyser_Main.java
+++ src/main/java/moodAnalyser_Main/Mood_analyser_Main.java
@@ -2,20 +2,20 @@ package moodAnalyser_Main;
 
 public class Mood_analyser_Main {
 	private String msg;
-	 public Mood_analyser_Main() {
-	    }
-	 public Mood_analyser_Main(String msg) {
-	        this.msg = msg;
-	    }
-	 public String analyseMood() throws Mood_Excep {
-	        try {
-	            if(msg.contains("SAD")){
-	                return "SAD";
-	            }
-	            return "HAPPY";
-	        }catch (NullPointerException p){
-	            return "HAPPY";
-         }
 
-	 }
+    //Default Constructor
+    public Mood_analyser_Main() {
+    }
+
+    //Parameterized Constructor
+    public Mood_analyser_Main(String msg) {
+        this.msg = msg;
+    }
+
+    public String analyseMood() {
+        if (msg.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
