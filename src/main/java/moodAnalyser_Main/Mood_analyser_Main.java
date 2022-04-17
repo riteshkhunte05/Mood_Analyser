9f6679d701674ded0cdb38ccfde6e72441ff2d9f
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-04-17 14:32:22 +0530

    UC2 containing TC2.1 program added

--- src/main/java/moodAnalyser_Main/Mood_analyser_Main.java
+++ src/main/java/moodAnalyser_Main/Mood_analyser_Main.java
@@ -1,14 +1,24 @@
 package moodAnalyser_Main;
 
 public class Mood_analyser_Main {
-
-	public String analyseMood(String msg) {
-		if(msg.contains(("sad"))) {
-			return "SAD";
-		}
-		else {
-			return "HAPPY";
-			
-		}
+	private String msg;
+	 public Mood_analyser_Main() {
+	    }
+	 public Mood_analyser_Main(String msg) {
+	        this.msg = msg;
+	    }
+public String analyseMood() {
+	try {
+        if (msg.contains("sad")) 
+        {
+            return "SAD";
+        }
+        else 
+        {
+            return "HAPPY";
+         }
+    } catch(Exception p) {
+        return "HAPPY";
 	}
+}
 }
--- src/test/java/moodAnalyserTest/MoodAnalyzerTest.java
+++ src/test/java/moodAnalyserTest/MoodAnalyzerTest.java
@@ -1,23 +1,30 @@
 package moodAnalyserTest;
 
-	import static org.junit.Assert.*;
-	import org.assertj.core.api.Assertions;
-	import org.junit.Test;
-    import moodAnalyser_Main.Mood_analyser_Main;
+import static org.junit.Assert.*;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
-	public class MoodAnalyzerTest {
-		    @Test
-		    public void testMood_MoodIsSad() {
-		    	Mood_analyser_Main ma = new Mood_analyser_Main();
-		        String mood = ma.analyseMood("I am in sad mood");
-		        assertEquals(mood,"SAD");
-		    }
-	
-		    @Test
-		    public void testMood_MoodIsHappy() {
-		    	Mood_analyser_Main ma = new Mood_analyser_Main();
-		        String mood1 = ma.analyseMood("I am in any mood");
-		        assertEquals(mood1,"HAPPY");    
-	}
-	}
+import moodAnalyser_Main.Mood_analyser_Main;
 
+public class MoodAnalyzerTest {
+	    @Test
+	    public void testMood_MoodIsSad() {
+	    	Mood_analyser_Main ma = new Mood_analyser_Main("I am in sad mood ");
+	        String mood = ma.analyseMood();
+	        assertEquals(mood,"SAD");
+	    }
+	    @Test
+	    public void testMood_MoodIsHappy() {
+	    	Mood_analyser_Main ma = new Mood_analyser_Main("I am in any mood ");
+	        String mood1 = ma.analyseMood();
+	        assertEquals(mood1,"HAPPY");
+	    }
+
+      @Test
+      public void Null() {
+    	  Mood_analyser_Main ma = new Mood_analyser_Main(null);
+      String mood2 = ma.analyseMood();
+    assertEquals(mood2,"HAPPY");
+
+}
+}
